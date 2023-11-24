929c6c1b51473e59d75ad8a54dbad1fd3ee144e2
Konrad <vobis08gw@gmail.com>
2023-11-24 12:22:23 +0100

    Update PageNavbar.tsx

--- gml-cheatsheet/src/PageNavbar.tsx
+++ gml-cheatsheet/src/PageNavbar.tsx
@@ -2,13 +2,14 @@ import Nav from "react-bootstrap/Nav";
 
 function PageNavbar() {
   return (
-    <Nav defaultActiveKey="/home" className="flex-column">
-      <Nav.Link href="/home">Active</Nav.Link>
-      <Nav.Link eventKey="link-1">Link</Nav.Link>
-      <Nav.Link eventKey="link-2">Link</Nav.Link>
-      <Nav.Link eventKey="disabled" disabled>
-        Disabled
-      </Nav.Link>
+    <Nav variant="pills" defaultActiveKey="variables" className="flex-column">
+      <Nav.Link eventKey="variables">Variables</Nav.Link>
+      <Nav.Link eventKey="arrays">Arrays</Nav.Link>
+      <Nav.Link eventKey="objects">Objects</Nav.Link>
+      <Nav.Link eventKey="functions">Functions</Nav.Link>
+      <Nav.Link eventKey="operators">Operators</Nav.Link>
+      <Nav.Link eventKey="loops">Loops</Nav.Link>
+      <Nav.Link eventKey="ifs">if, else, switch</Nav.Link>
     </Nav>
   );
 }
